5a80fa60916c7efe33be89d7737ed301a3e16465
Teuuz1994 <matheus.rj.ig@gmail.com>
2020-07-30 19:03:51 -0300

    feat: create routes of list appointments and create a new appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,5 +1,37 @@
 import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRoutes = Router();
+
+appointmentsRoutes.get('/', async (request, response) => {
+  const appointmentRepository = getCustomRepository(AppointmentRepository);
+
+  const appointments = await appointmentRepository.find();
+
+  return response.json(appointments);
+});
+
+appointmentsRoutes.post('/', async (request, response) => {
+  try {
+    const { date, provider } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointmentService = new CreateAppointmentService();
+
+    const appointment = await createAppointmentService.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return response.json(appointment);
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
 
 export default appointmentsRoutes;
