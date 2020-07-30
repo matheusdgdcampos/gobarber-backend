024b8e6bc57e9edc4104b2bc363e6f1b377e4348
Teuuz1994 <matheus.rj.ig@gmail.com>
2020-07-30 18:30:22 -0300

    feat: make model of appontments

--- src/database/migrations/1596142478232-CreateAppointments.ts
+++ src/database/migrations/1596142478232-CreateAppointments.ts
@@ -22,6 +22,16 @@ export default class CreateAppointments1596142478232
             name: 'date',
             type: 'timestamp with time zone',
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
