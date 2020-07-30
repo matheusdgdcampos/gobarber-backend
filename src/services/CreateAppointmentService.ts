b02e3e511134c8d9b40c928947823b7ed053ce73
Teuuz1994 <matheus.rj.ig@gmail.com>
2020-07-30 18:51:33 -0300

    feat: created appointment repository and make service at appointment creation

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,37 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import Appointment from '../models/Appointment';
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({ date, provider }: Request): Promise<Appointment> {
+    const appointmentRepository = getCustomRepository(AppointmentRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new Error('This appointment is already booked');
+    }
+
+    const appointment = appointmentRepository.create({
+      date: appointmentDate,
+      provider,
+    });
+
+    await appointmentRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
