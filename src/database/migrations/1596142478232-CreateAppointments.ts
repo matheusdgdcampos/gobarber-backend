ee16dd007b41b2d5a5700ec508185f4250162ac4
Teuuz1994 <matheus.rj.ig@gmail.com>
2020-07-30 21:45:55 -0300

    feat: create a new migration for added forengnKey in colum appointments

--- src/database/migrations/1596142478232-CreateAppointments.ts
+++ src/database/migrations/1596142478232-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1596142478232
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
