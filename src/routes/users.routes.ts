839335f8437cbc7f2bf9f156f53ab8fd28b0d153
Teuuz1994 <matheus.rj.ig@gmail.com>
2020-08-01 00:33:33 -0300

    feat: added router for update avatar in user account

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
-
-usersRouter.use(ensureAuthenticated);
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -23,5 +24,14 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: error.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
