26cb3e634f23db7ecf30384caee62c4b1da56c57
Teuuz1994 <matheus.rj.ig@gmail.com>
2020-08-01 19:12:24 -0300

    feat(backend): serve static files on route /files

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import express from 'express';
 
+import uploadConfig from './config/upload';
+
 import './database';
 
 import Routes from './routes';
@@ -9,6 +11,7 @@ const app = express();
 const port = 3333;
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(Routes);
 
 app.listen(port, () => console.log('server is running on port 3333'));
