49e2cb023ccf435c2db228fee4d73203f44fd138
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2024-12-23 19:18:04 -0300

    feat: add PrismaService to AppointmentsModule providers

--- src/modules/appointments/appointments.module.ts
+++ src/modules/appointments/appointments.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppointmentsService } from './appointments.service';
 import { AppointmentsController } from './appointments.controller';
 import { AppointmentsRepository } from './appointment.repository';
+import { PrismaService } from '@common/services/prisma.service';
 
 @Module({
   controllers: [AppointmentsController],
-  providers: [AppointmentsService, AppointmentsRepository],
+  providers: [AppointmentsService, AppointmentsRepository, PrismaService],
   exports: [AppointmentsService],
 })
 export class AppointmentsModule {}
