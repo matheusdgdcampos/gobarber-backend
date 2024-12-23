30cfaccfb5a490d065b614a1d133209df48cbe12
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2024-12-23 19:18:27 -0300

    feat: add CommonModule and environment validation to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppointmentsModule } from './modules/appointments/appointments.module';
+import { validate } from './config/environments-config';
+import { CommonModule } from '@common/modules/common.modules';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validate,
     }),
+    CommonModule,
     AppointmentsModule,
   ],
   controllers: [],
