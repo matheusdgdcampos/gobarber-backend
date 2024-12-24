0149e20b439c271e577ccb59164a4f476f723f46
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2024-12-24 17:37:10 -0300

    refactor: update appointment creation DTO import for consistency

--- src/modules/appointments/appointments.controller.ts
+++ src/modules/appointments/appointments.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppointmentsService } from './appointments.service';
-import { CreateAppointmentDto } from './dto/create-appointment.dto';
+import { AppointmentCreateRequestDto } from './dto/appointment-create-request.dto';
 
 @Controller('appointments')
 export class AppointmentsController {
   constructor(private readonly appointmentsService: AppointmentsService) {}
 
   @Post()
-  create(@Body() createAppointmentDto: CreateAppointmentDto) {
+  create(@Body() createAppointmentDto: AppointmentCreateRequestDto) {
     return this.appointmentsService.create(createAppointmentDto);
   }
 
