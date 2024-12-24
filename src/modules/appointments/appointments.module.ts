b88f642f90f93c23a9e8814a1ecea13d97f9c2db
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2024-12-24 17:36:57 -0300

    refactor: remove unused PrismaService from AppointmentsModule providers

--- src/modules/appointments/appointments.module.ts
+++ src/modules/appointments/appointments.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppointmentsService } from './appointments.service';
 import { AppointmentsController } from './appointments.controller';
 import { AppointmentsRepository } from './appointment.repository';
-import { PrismaService } from '@common/services/prisma.service';
 
 @Module({
   controllers: [AppointmentsController],
-  providers: [AppointmentsService, AppointmentsRepository, PrismaService],
+  providers: [AppointmentsService, AppointmentsRepository],
   exports: [AppointmentsService],
 })
 export class AppointmentsModule {}
