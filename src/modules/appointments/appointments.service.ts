e511567af90b22a8dde674d5298df875e0eca2fa
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2024-12-24 17:36:43 -0300

    feat: enhance appointment creation with error handling and logging

--- src/modules/appointments/appointments.service.ts
+++ src/modules/appointments/appointments.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  Logger,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { parseISO, startOfHour } from 'date-fns';
 import { AppointmentsRepository } from './appointment.repository';
 import { Appointments } from './entities/appointment.entity';
@@ -6,30 +11,47 @@ import { AppointmentCreateRequestDto } from './dto/appointment-create-request.dt
 
 @Injectable()
 export class AppointmentsService {
+  private logger = new Logger(AppointmentsService.name);
+
   constructor(protected appointmentsRepository: AppointmentsRepository) {}
 
   public async create(
     createAppointmentDto: AppointmentCreateRequestDto,
   ): Promise<Appointments> {
-    const parsedDate = startOfHour(parseISO(createAppointmentDto.date));
-    const findApppointmentInSameDate =
-      this.appointmentsRepository.findByDate(parsedDate);
+    try {
+      const parsedDate = startOfHour(parseISO(createAppointmentDto.date));
+      const findApppointmentInSameDate =
+        await this.appointmentsRepository.findByDate(parsedDate);
 
-    if (findApppointmentInSameDate) {
-      throw new BadRequestException({
-        statusCode: 400,
-        message: 'This appointment is already booked',
+      if (findApppointmentInSameDate) {
+        throw new BadRequestException({
+          statusCode: 400,
+          message: 'This appointment is already booked',
+        });
+      }
+
+      const appointment = await this.appointmentsRepository.create({
+        providerId: createAppointmentDto.providerId,
+        date: parsedDate,
       });
-    }
+      return appointment;
+    } catch (error) {
+      this.logger.error(error);
 
-    const appointment = await this.appointmentsRepository.create({
-      provider: createAppointmentDto.provider,
-      date: parsedDate,
-    });
-    return appointment;
+      if (error instanceof BadRequestException) {
+        throw error;
+      }
+
+      throw new UnprocessableEntityException();
+    }
   }
 
   public findAll() {
-    return this.appointmentsRepository.findAll();
+    try {
+      return this.appointmentsRepository.findAll();
+    } catch (error) {
+      this.logger.error(error);
+      throw new UnprocessableEntityException();
+    }
   }
 }
