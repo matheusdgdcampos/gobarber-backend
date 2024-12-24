d6a522cf053a670e5e6f807ded446cc8cd77d519
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2024-12-24 17:50:16 -0300

    feat: add HashService for password hashing and comparison

--- src/common/modules/common.modules.ts
+++ src/common/modules/common.modules.ts
@@ -1,9 +1,10 @@
+import { HashService } from '@common/services/hash.service';
 import { PrismaService } from '@common/services/prisma.service';
 import { Global, Module } from '@nestjs/common';
 
 @Global()
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [PrismaService, HashService],
+  exports: [PrismaService, HashService],
 })
-export class CommonModule {}
+export class CommonModule { }
