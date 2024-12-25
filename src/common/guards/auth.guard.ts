f9698172ec43c98d7f3af1d6d5dc832bb4cc112d
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2024-12-25 12:29:51 -0300

    feat: implement AuthGuard to protect routes using JWT authentication

---
+++ src/common/guards/auth.guard.ts
@@ -0,0 +1,40 @@
+import { EnvironmentVariables } from '@config/environments-config';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    protected readonly jwtService: JwtService,
+    protected readonly configService: ConfigService<EnvironmentVariables>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.getOrThrow('JWT_SECRET'),
+      });
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
