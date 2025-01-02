d92f71ff7f6b41853960a09284b305ec8e671f2b
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2025-01-02 06:00:18 -0300

    feat: enhance app module with TypeORM PostgreSQL configuration and improved ConfigModule options

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,39 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppointmentsModule } from './modules/appointments/appointments.module';
-import { validate } from './config/environments-config';
-import { CommonModule } from '@common/modules/common.modules';
+import { EnvironmentVariables, validate } from './config/environments-config';
 import { UsersModule } from './modules/users/users.module';
-import { PrismaService } from '@common/services/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from '@common/modules/common.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validate,
+      cache: true,
+      expandVariables: true,
     }),
     JwtModule.register({
       global: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService<EnvironmentVariables>) => ({
+        type: 'postgres',
+        port: 5432,
+        url: configService.getOrThrow('DATABASE_URL'),
+        synchronize:
+          configService.getOrThrow('NODE_ENV') === 'development' ? true : false,
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
     }),
     CommonModule,
     AppointmentsModule,
     UsersModule,
     AuthModule,
   ],
-  providers: [PrismaService],
 })
 export class AppModule {}
